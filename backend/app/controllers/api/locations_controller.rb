08a111db75b1cbcae06fa11a356264e11fba6e45
jpatamian <jpatamian@gmail.com>
2024-02-29 12:03:31 -0500

    later on updates

--- backend/app/controllers/api/locations_controller.rb
+++ backend/app/controllers/api/locations_controller.rb
@@ -1,9 +1,16 @@
 class Api::LocationsController < ApplicationController
   def index
     # low level caching using the cache_key which is a hash of the max updated_at
-    locations = Rails.cache.fetch(Location.cache_key) do
-      Location.all.order(:name)
+    # locations = Rails.cache.fetch(Location.cache_key) do
+    #   Location.all.order(:name)
+    # end
+
+    # I think the max updated at was the wrong way to go here and I should have used a time cache because the weather doesn't change much after 1 minute or so
+    location_ids = Rails.cache.fetch('location_ids', expires_in: 1.minute) do
+      Location.all.pluck(:id)
     end
+
+    locations = Location.find(location_ids).order(:name)
 
     render json: locations, status: :ok
   rescue StandardError => e
